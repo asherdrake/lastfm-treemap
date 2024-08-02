734c79182dfc0feb478bb177a30978b9806e51f8
asherdrake <asher.alacar@gmail.com>
2024-08-02 16:40:59 -0700

    docker

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { DatasetComponent } from './dataset/dataset.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   @ViewChild('treemapPlaceholder', { read: ViewContainerRef }) treemapContainer!: ViewContainerRef;
